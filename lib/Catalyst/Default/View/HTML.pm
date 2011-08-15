33a252704b965c93847905ba4922ea1f5e91cf6a
Phillip Smith <ps@phillipadsmith.com>
2011-08-15 16:23:36 -0400

    Created View::HTML and set the component path

---
+++ lib/Catalyst/Default/View/HTML.pm
@@ -0,0 +1,37 @@
+package Catalyst::Default::View::HTML;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    # Change default TT extension
+    TEMPLATE_EXTENSION => '.tt2',
+    render_die => 1,
+);
+
+=head1 NAME
+
+Catalyst::Default::View::HTML - TT View for Catalyst::Default
+
+=head1 DESCRIPTION
+
+TT View for Catalyst::Default.
+
+=head1 SEE ALSO
+
+L<Catalyst::Default>
+
+=head1 AUTHOR
+
+Phillip Smith
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ t/view_HTML.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::More;
+
+BEGIN { use_ok 'Catalyst::Default::View::HTML' }
+
+done_testing();
